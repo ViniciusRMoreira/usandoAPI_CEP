0e59e39b741995e2d2a6f69b7a0b36745adf2cb7
Vinicius Rodolfo <viniciusrodolfomoreira@hotmail.com>
2021-09-15 21:04:33 -0300

    Validacao e Mascara adicionados

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,24 +13,46 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  String _resultado = "Resultado";
+  final _formKey = GlobalKey<FormState>();
+  String _resultado = "";
   TextEditingController _controllerCep = TextEditingController();
+
+  var mascaraCep = MaskTextInputFormatter(mask: '########', filter: {
+    "#": RegExp(
+      r'[0-9]',
+    )
+  });
+
+  _validar() {
+    if (_formKey.currentState!.validate() == true) {
+      _recuperarCep();
+    } else {
+      print("Erro");
+    }
+  }
+
   _recuperarCep() async {
     String cepDigitado = _controllerCep.text;
-    String url = 'http://viacep.com.br/ws/${cepDigitado}/json/';
+    String url = 'http://viacep.com.br/ws/$cepDigitado/json/';
 
     http.Response response;
 
     response = await http.get(Uri.parse(url));
 
     print("Resposta: " + response.body);
-    Map<String, dynamic> retorno = json.decode(response.body);
-    String localidade = retorno["localidade"];
-    String cep = retorno["cep"];
-    String uf = retorno["uf"];
+    Map<String, dynamic> retorno = json.decode(response.body) ?? "";
 
+    var localidade = retorno["localidade"] ?? "";
+    var cep = retorno["cep"] ?? "CEP INCORRETO";
+    var uf = retorno["uf"] ?? "";
+    var bairro = retorno["bairro"] ?? "";
+    var ddd = retorno["ddd"] ?? "";
+    var logradouro = retorno["logradouro"] ?? "";
+    print("Resposta" + response.body);
+    var local =
+        "Cep: $cep\nLocalidade: $localidade\nLogradouro:$logradouro\nUF: $uf\nBairro:$bairro\nDDD:$ddd";
     setState(() {
-      _resultado = "Cep: ${cep}\nLocalidade: ${localidade}\nUF: ${uf}";
+      _resultado = local;
     });
   }
 
@@ -35,31 +60,51 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Consumo serviço web"),
+        title: Text("Consultando CEP via API"),
       ),
       body: Container(
         padding: EdgeInsets.all(40),
-        child: Column(
-          children: [
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Digite o cep: ex:12530000",
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Não pode ser vazio";
+                        }
+                        if (value.length < 8) {
+                          return "CEP incompleto";
+                        }
+                      },
+                      inputFormatters: [mascaraCep],
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: "Digite o CEP:",
+                        hintText: '00000-000',
+                      ),
+                      style: TextStyle(fontSize: 20),
+                      controller: _controllerCep,
+                    ),
+                    ElevatedButton(
+                      onPressed: _validar,
+                      child: Text(
+                        "Conferir CEP",
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 30)),
+                    Text(
+                      _resultado,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
               ),
-              style: TextStyle(fontSize: 20),
-              controller: _controllerCep,
-            ),
-            RaisedButton(
-              onPressed: _recuperarCep,
-              child: Text(
-                "Clique aqui",
-              ),
-            ),
-            Text(
-              _resultado,
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
